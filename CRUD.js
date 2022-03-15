260120f2cd4a2ad2c60852f6acd49a5c1d6d3e5c
Daniel Sam <daniellevilucas@gmail.com>
2022-03-16 02:13:56 +0530

    createStudentData() Added

---
+++ CRUD.js
@@ -0,0 +1,27 @@
+const readline = require("readline-sync");
+const chalk = require("chalk");
+const jsonReadWrite = require("jsonReadWrite");
+
+function createStudentData() {
+  studentData = jsonReadWrite.jsonRead("student.json");
+
+  const studentDB = {
+    studentID,
+    firstName,
+    lastName,
+    DOB,
+    department,
+  };
+
+  Object.keys(studentDB).forEach((key) => {
+    studentDB[key] = readline.question(chalk.white(`Enter student ${key} :`));
+    process.stdout.write("\n");
+  });
+  studentData.push(studentDB);
+
+  jsonReadWrite.josnWrite(studentDB);
+}
+
+module.exports = {
+  createStudentData: createStudentData,
+};
