f11874bddbb35bab7e8a33d238d900c3fcf91e4a
Daniel Sam <daniellevilucas@gmail.com>
2022-03-16 02:20:11 +0530

    printMenu() added

---
+++ app.js
@@ -0,0 +1,25 @@
+const readline = require("readline-sync");
+const chalk = require("chalk");
+const CRUD = require("./CRUD.js");
+
+function printMenu() {
+  process.stdout.write(
+    chalk.bgBlue.underline.black(`\n| ----------------------------------- |`)
+  );
+
+  process.stdout.write(
+    chalk.bgWhite.underline.white(`\n| ----------------------------------- |`)
+  );
+
+  process.stdout.write(
+    chalk.bgWhite.black(`
+  | Add    - To create a new user       |
+  | remove - To remove an existing user |
+  | update - To update an existing user |
+  | Show   - To show an existing users  |
+
+  `)
+  );
+}
+
+printMenu();
