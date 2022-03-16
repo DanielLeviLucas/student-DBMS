19c8bbb5f41ac76598ad23c2fd41393632c94f52
Daniel Sam <daniellevilucas@gmail.com>
2022-03-16 22:48:25 +0530

    remove student record functionality added

--- CRUD.js
+++ CRUD.js
@@ -22,6 +22,22 @@ function createStudentData() {
   jsonReadWrite.jsonWrite(studentData);
 }
 
+function removeStudentRecord(id) {
+  studentData = jsonReadWrite.jsonRead();
+
+  const index = studentData.findIndex((element) => {
+    return element.studentID === id.toUpperCase();
+  });
+
+  if (index != -1) {
+    studentData.splice(index, 1);
+    console.log(chalk.bgGreen.black("Record successfully removed"));
+    jsonReadWrite.jsonWrite(studentData);
+  } else {
+    console.log(chalk.white.bgRed("No match found"));
+  }
+}
+
 function showAllStudentData() {
   studentData = jsonReadWrite.jsonRead();
   console.table(studentData);
@@ -30,4 +46,5 @@ function showAllStudentData() {
 module.exports = {
   createStudentData: createStudentData,
   showAllStudentData: showAllStudentData,
+  removeStudentRecord: removeStudentRecord,
 };
